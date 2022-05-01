388ff8f97d7a81baa0314f7b50541f5dc1ed9249
Olatunde Rasheed <65951629+olatunde1998@users.noreply.github.com>
2022-05-01 05:39:15 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,84 @@
+//const hamburger = document.getElementById('hamburger');
+//const navUL = document.getElementById('nav-ul');
+
+//hamburger.addEventListener('click', function(){
+    //navUL.classList.toggle('nav-ul');
+//});
+
+
+//var x = document.getElementsById("collections");
+
+
+var keypad = document.querySelector('.keypad');
+var display = document.querySelector('.display-box')
+var seven = document.querySelector('.seven')
+var eight = document.querySelector('.eight')
+var nine = document.querySelector('.nine')
+var delet = document.querySelector('.delet')
+var four = document.querySelector('.four')
+var five = document.querySelector('.five')
+var six = document.querySelector('.six')
+var add = document.querySelector('.add')
+var one = document.querySelector('.one')
+var two = document.querySelector('.two')
+var three = document.querySelector('.three')
+var subtract = document.querySelector('.subtact')
+var dot = document.querySelector('.dot')
+var zero = document.querySelector('.zero')
+var divide = document.querySelector('.divide')
+var multiply = document.querySelector('.multiply')
+var reset = document.querySelector('.reset')
+var equalto = document.querySelector('.equalto')
+
+
+seven.onclick = function(){
+  display.innerHTML = 7;
+}
+
+eight.onclick = function(){
+  display.innerHTML = 8;
+}
+nine.onclick = function(){
+  display.innerHTML = 9;
+}
+delet.onclick = function(){
+  display.innerHTML = 0;
+}
+four.onclick = function(){
+  display.innerHTML = 4;
+}
+five.onclick = function(){
+  display.innerHTML = 5;
+}
+six.onclick = function(){
+  display.innerHTML = 6;
+}
+one.onclick = function(){
+  display.innerHTML = 1;
+}
+two.onclick = function(){
+  display.innerHTML = 2;
+}
+three.onclick = function(){
+  display.innerHTML = 3;
+}
+zero.onclick = function(){
+  display.innerHTML = 0;
+}
+reset.onclick = function(){
+  display.innerHTML = 0;
+}
+//if ('seven', '+', 'eight'){
+  //display.innerHTML = 1;
+//}
+
+
+
+
+
+
+
+
+
+
+  
